9d9aafd2e18e2a39a7ed10bb4e2255c177c09d17
Uz0r0 <uzar.keneshbekov@gmail.com>
2025-05-19 12:58:15 +0600

    Add files via upload

---
+++ src/app/routers.jsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import ContactPage from "../pages/ContactPage/ContactPage";
+import AboutPage from "../pages/AboutPage/AboutPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
+import LogInPage from "../pages/LogInPage/LogInPage";
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import BattlePage from "../pages/BattlePage/BattlePage";
+import SettingsPage from "../pages/SettingsPage/SettingsPage";
+import AfterReg from "../pages/AfterReg/AfterReg";
+import SetPasswordPage from "../pages/SetPasswordPage/SetPasswordPage";
+
+export const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LogInPage />,
+  },
+  {
+    path: "/profile/:username",
+    element: <ProfilePage />,
+  },
+  {
+    path: "/battle",
+    element: <BattlePage />,
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage />,
+  },
+  {
+    path: "/staring",
+    element: <AfterReg />,
+  },
+  {
+    path: "/setPassword",
+    element: <SetPasswordPage />,
+  }
+]);
---
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from '../../components/Style.module.css'
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+
+function SettingsPage() {
+    const [music, setMusic] = useState("");
+    const [musician, setMusician] = useState("");
+    const [actor, setActor] = useState("");
+    const [anime, setAnime] = useState("");
+    const [film, setFilm] = useState("");
+    const [meme, setMeme] = useState("");
+    const [message, setMessage] = useState("");
+    
+    const Picks = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem("access_token");
+
+            if (!token) {
+                setMessage("Not authenticated. Please log in.");
+                return;
+            }
+
+            const res = await axios.put("http://localhost:8080/card/change",
+                { music, musician, actor, anime, film, meme },
+                {
+                    headers: { 
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            setMessage("The update was successful");
+        } catch (error) {
+            console.error("Error:", error.response?.data || error.message);
+            setMessage("The update was not successful");
+        }
+};
+        
+    return (
+      <>
+        <div>
+            <div className={styles.HeaderBack}>
+                <Header />
+            </div>
+            <div className={styles.VerticalLine}></div>
+            <div className={`${styles.gradientBackProfile} ${styles.center}`}>
+                <div className={styles.textWrapper}>
+                    <h1 className={styles.a}>Editing your profile</h1>
+                    <a href={`/profile/${localStorage.getItem("username")}`}>Go to profile</a>
+                </div>
+                <div className={styles.PicksCard}>
+                    <form onSubmit={Picks} >
+                        <div className={styles.formContainer}>
+                            <a href="/setPassword" className={styles.passwordStyle}>Change password</a>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setMusic(e.target.value)}
+                                    value={music}
+                                    type="text"
+                                    placeholder="Music"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setMusician(e.target.value)}
+                                    value={musician}
+                                    type="text"
+                                    placeholder="Musician"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setActor(e.target.value)}
+                                    value={actor}
+                                    type="text"
+                                    placeholder="Actor"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setAnime(e.target.value)}
+                                    value={anime}
+                                    type="text"
+                                    placeholder="Anime"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setFilm(e.target.value)}
+                                    value={film}
+                                    type="text"
+                                    placeholder="Film"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                            <div className={styles.inputStyleS}>
+                                <input
+                                    onChange={(e) => setMeme(e.target.value)}
+                                    value={meme}
+                                    type="text"
+                                    placeholder="Meme"
+                                    className={styles.inputField}
+                                />
+                            </div>
+                        </div> 
+                        {message && <div className={styles.UpdateMessage}>{message}</div>}
+                        <button type="submit" className={styles.btnStyle}>Save changes</button> 
+                    </form>
+                </div>
+            </div>
+            <div className={styles.VerticalLine}></div>
+            <div className={styles.footerBack}>
+                <Footer />
+            </div> 
+        </div>
+      </>
+    );
+};
+ 
+export default SettingsPage
+
+
